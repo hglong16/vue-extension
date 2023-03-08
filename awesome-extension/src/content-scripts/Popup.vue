802682ced2cbb2cc9721c616801b3b2889ba9912
hglong16 <intihad.vuong@gmail.com>
2023-03-08 09:35:19 +0700

    init

---
+++ awesome-extension/src/content-scripts/Popup.vue
@@ -0,0 +1,24 @@
+<template lang="">
+        <div class="overlay">
+            <div class="popup">
+              <h1>Legen .. wait for it.. dary</h1>
+
+            </div>
+        </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup() {
+    const visible = ref(false)
+    return {
+      visible
+    }
+  }
+})
+</script>
+<style lang="">
+.overlay {
+  @apply fixed  inset-0 w-full h-full bg-black bg-opacity;
+}
+</style>
